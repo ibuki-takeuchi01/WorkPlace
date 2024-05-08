b6cc01b42c168c4fbd3ce9ccf3b4a9f2bd39aa93
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-05-08 22:31:48 +0900

    DockerでMySQLをセットアップ

--- reversi-app/src/main.ts
+++ reversi-app/src/main.ts
@@ -1,6 +1,7 @@
-import express from "express"
-import morgan from "morgan"
+import express from 'express'
 import 'express-async-errors'
+import morgan from 'morgan'
+import mysql from 'mysql2/promise'
 
 const PORT = 3000
 
@@ -11,12 +12,34 @@ app.use(express.static('static', { extensions: ['html'] }))
 
 app.get('/api/hello', async (req, res) => {
   res.json({
-    message: 'Hello Express'
+    message: 'Hello Express!!!'
   })
 })
 
 app.get('/api/error', async (req, res) => {
   throw new Error('Error endpoint')
+})
+
+app.post('/api/games', async (req, res) => {
+  const now = new Date()
+
+  const conn = await mysql.createConnection({
+    host: 'localhost',
+    database: 'reversi',
+    user: 'reversi',
+    password: 'password'
+  })
+  try {
+    await conn.beginTransaction()
+
+    await conn.execute('insert into games (started_at) values (?)', [now])
+
+    await conn.commit()
+  } finally {
+    await conn.end()
+  }
+
+  res.status(201).end()
 })
 
 app.use(errorHandler)
@@ -25,9 +48,14 @@ app.listen(PORT, () => {
   console.log(`Reversi application started: http://localhost:${PORT}`)
 })
 
-function errorHandler(error: any, _req: express.Request, res: express.Response, _next: express.NextFunction){
-  console.error('Unexpected error occurred', error)
+function errorHandler(
+  err: any,
+  _req: express.Request,
+  res: express.Response,
+  _next: express.NextFunction
+) {
+  console.error('Unexpected error occurred', err)
   res.status(500).send({
-    messsage: 'Unexpected error occurred'
+    message: 'Unexpected error occurred'
   })
 }
