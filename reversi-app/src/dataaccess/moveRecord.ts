6e37a5cd61bb0ff3815126b2e33889ac9dddbc78
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-05-27 19:39:56 +0900

    turnRepository 実装

--- reversi-app/src/dataaccess/moveRecord.ts
+++ reversi-app/src/dataaccess/moveRecord.ts
@@ -1,9 +1,21 @@
-export class moveRecord{
+export class MoveRecord{
   constructor(
     private _id: number,
     private _turnId: number,
-    private _desc: number,
+    private _disc: number,
     private _x: number,
     private _y: number
   ){}
+
+  get disc() {
+    return this._disc
+  }
+
+  get x() {
+    return this._x
+  }
+
+  get y() {
+    return this._y
+  }
 }
