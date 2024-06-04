6493e8b0891a5f2f712e67d4d873b5b6b970a1b0
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-06-04 20:17:05 +0900

    フォルダ構成 修正
    Service→UseCase

--- reversi-app/src/application/useCase/findLatestGameTurnByTurnCountUseCase.ts
+++ reversi-app/src/application/useCase/findLatestGameTurnByTurnCountUseCase.ts
@@ -1,6 +1,4 @@
 import { connectMySQL } from '../../infrastructure/connection'
-import { Disc } from '../../domain/model/turn/disc'
-import { Point } from '../../domain/model/turn/point'
 import { ApplicationError } from '../error/applicationError'
 import { GameResult } from '../../domain/model/gameResult/gameResult'
 import { TurnRepository } from '../../domain/model/turn/turnRepository'
@@ -32,15 +30,14 @@ class FindLatestGameTurnByTurnCountOutput {
   }
 }
 
-export class TurnService {
-
+export class FindLatestGameTurnByTurnCountUseCase {
   constructor(
     private _turnRepository: TurnRepository,
     private _gameRepository: GameRepository,
     private _gameResultRepository: GameResultRepository
   ) {}
 
-  async findLatestGameTurnByTurnCount(turnCount: number): Promise<FindLatestGameTurnByTurnCountOutput>{
+  async run(turnCount: number): Promise<FindLatestGameTurnByTurnCountOutput>{
     const conn = await connectMySQL()
   try {
     const game = await this._gameRepository.findLatest(conn)
@@ -71,47 +68,4 @@ export class TurnService {
   } finally {
     await conn.end()
   }}
-
-  async registerTurn(
-    turnCount: number,
-    disc: Disc,
-    point: Point,
-  ){
-    const conn = await connectMySQL()
-  try {
-    const game = await this._gameRepository.findLatest(conn)
-    if(!game){
-      throw new ApplicationError('LatestGameNotFound' ,'Latest game not found')
-    }
-    if (!game.id) {
-      throw new Error('game.id not exist')
-    }
-
-    const previousTurnCount = turnCount - 1
-    const previousTurn = await this._turnRepository.findGameIdAndTurnCount(
-      conn,
-      game.id,
-      previousTurnCount
-    )
-
-    // 石を置く
-    const newTurn = previousTurn.placeNext(disc, point)
-
-    // ターンを保存する
-    await this._turnRepository.save(conn, newTurn)
-
-    // 勝敗が決した場合、対戦結果を保存
-    if (newTurn.gameEnded()) {
-      const winnerDisc = newTurn.winnerDisc()
-      const gameResult = new GameResult(game.id, winnerDisc, newTurn.endAt)
-      await this._gameResultRepository.save(conn, gameResult)
-
-      // 対戦結果を保存
-    }
-
-    await conn.commit()
-  } finally {
-    await conn.end()
-  }
-  }
 }
--- reversi-app/src/presentation/turnRouter.ts
+++ reversi-app/src/presentation/turnRouter.ts
@@ -1,14 +1,21 @@
 import express from 'express'
-import { TurnService } from '../application/service/turnService'
+import { FindLatestGameTurnByTurnCountUseCase } from '../application/useCase/findLatestGameTurnByTurnCountUseCase'
 import { Point } from '../domain/model/turn/point'
 import { toDisc } from '../domain/model/turn/disc'
 import { TurnMySQLRepository } from '../infrastructure/repository/turn/turnMySQLRepository'
 import { GameMySQLRepository } from '../infrastructure/repository/game/gameMySQLRepository'
 import { GameResultMySQLRepository } from '../infrastructure/repository/gameResult/gameResultMySQLRepository'
+import { RegisterTurnUseCase } from '../application/useCase/registerTurnUseCase'
 
 export const turnRouter = express.Router()
 
-const turnService = new TurnService(
+const findLatestGameTurnByTurnCountUseCase = new FindLatestGameTurnByTurnCountUseCase(
+  new TurnMySQLRepository,
+  new GameMySQLRepository,
+  new GameResultMySQLRepository
+)
+
+const registerTurnUseCase = new RegisterTurnUseCase(
   new TurnMySQLRepository,
   new GameMySQLRepository,
   new GameResultMySQLRepository
@@ -24,7 +31,7 @@ interface TurnGetResponseBody{
 turnRouter.get('/api/games/latest/turns/:turnCount', async (req, res: express.Response<TurnGetResponseBody>) => {
   const turnCount = parseInt(req.params.turnCount)
 
-  const output = await turnService.findLatestGameTurnByTurnCount(turnCount)
+  const output = await findLatestGameTurnByTurnCountUseCase.run(turnCount)
   const responseBody = {
     turnCount: output.turnCount,
     board: output.board,
@@ -48,7 +55,7 @@ turnRouter.post('/api/games/latest/turns', async (req: express.Request<{}, {}, t
   const disc = toDisc(req.body.move.disc)
   const point = new Point(req.body.move.x, req.body.move.y)
 
-  await turnService.registerTurn(turnCount, disc, point)
+  await registerTurnUseCase.run(turnCount, disc, point)
 
   res.status(201).end()
 })
