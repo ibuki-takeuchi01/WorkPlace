532a6e10efd80ebfc5a6411b7a49dd5a50336bb0
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-06-29 10:16:45 +0900

    APIから記事を取得

--- ExpoNewsApp/news-app/App.js
+++ ExpoNewsApp/news-app/App.js
@@ -1,19 +1,27 @@
+import { useState, useEffect } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
-import { ListItem } from "./components/ListItem"
-import articles from "./components/dummies/articles.json"
+import { ListItem } from "./components/ListItem";
+import axios from "axios";
+
+const URL =
+  "https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=2c5d780d6ec14a3583025692d3045ce3";
 
 export default function App() {
-  const items = articles.map((article, index) => {
-    return (
-      <ListItem
-        imageUrl={article.urlToImage}
-        title={article.title}
-        author={article.author}
-        key={index.toString()}
-      />
-    )
-  })
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchArticles()
+  }, [])
 
   return (
     <SafeAreaView style={styles.container}>
