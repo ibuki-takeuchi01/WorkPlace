22fc893876902056ff3e24cd53c93de04ecfefba
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-07-15 11:22:33 +0900

    UserScreen コンポーネント追加

---
+++ Shop-Review/shop-review-app/src/screens/UserScreen.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, SafeAreaView, Text } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../types/navigation";
+import { RouteProp } from "@react-navigation/native";
+
+type Props = {
+  navigation: StackNavigationProp<RootStackParamList, "User">;
+  route: RouteProp<RootStackParamList, "User">;
+};
+
+export const UserScreen: React.FC<any> = ({ navigation, route }: any) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>User Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+})
