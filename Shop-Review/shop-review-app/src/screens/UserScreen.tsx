fd1bec901e758e0367535d366d274cdcd0f851bb
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-07-19 08:27:40 +0900

    ユーザー画面 修正
    ・入力フォーム追加
    ・「保存する」ボタン追加

--- Shop-Review/shop-review-app/src/screens/UserScreen.tsx
+++ Shop-Review/shop-review-app/src/screens/UserScreen.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, SafeAreaView, Text } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../types/navigation";
 import { RouteProp } from "@react-navigation/native";
+import { Form } from "../components/Form";
+import { Button } from "../components/Button";
 
 type Props = {
   navigation: StackNavigationProp<RootStackParamList, "User">;
@@ -10,9 +12,15 @@ type Props = {
 };
 
 export const UserScreen: React.FC<any> = ({ navigation, route }: Props) => {
+  const [name, setName] = useState<string>("");
   return (
     <SafeAreaView style={styles.container}>
-      <Text>User Screen</Text>
+      <Form
+        value={name}
+        onChangeText={(text) => { setName(text) }}
+        label="名前"
+      />
+      <Button onPress={() => { }} text="保存する" />
     </SafeAreaView>
   );
 };
