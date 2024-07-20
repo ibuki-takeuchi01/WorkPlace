eaa5bf5c06f651a662e27b099050c91c6def9bc8
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-07-20 09:59:07 +0900

    User情報 更新

--- Shop-Review/shop-review-app/src/screens/UserScreen.tsx
+++ Shop-Review/shop-review-app/src/screens/UserScreen.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { StyleSheet, SafeAreaView, Text } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../types/navigation";
 import { RouteProp } from "@react-navigation/native";
 import { Form } from "../components/Form";
 import { Button } from "../components/Button";
+import { UserContext } from "../contexts/userContext";
+import { updateUser } from "../lib/firebase";
 
 type Props = {
   navigation: StackNavigationProp<RootStackParamList, "User">;
@@ -12,7 +14,15 @@ type Props = {
 };
 
 export const UserScreen: React.FC<any> = ({ navigation, route }: Props) => {
+  const { user } = useContext(UserContext);
   const [name, setName] = useState<string>("");
+
+
+  const onSubmit = async () => {
+    if (user == null) return;
+    await updateUser(user.id, name);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Form
@@ -20,7 +30,7 @@ export const UserScreen: React.FC<any> = ({ navigation, route }: Props) => {
         onChangeText={(text) => { setName(text) }}
         label="名前"
       />
-      <Button onPress={() => { }} text="保存する" />
+      <Button onPress={onSubmit} text="保存する" />
     </SafeAreaView>
   );
 };
