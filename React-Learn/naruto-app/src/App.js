62e6ffa3247e4084f2e31705e08b3575a8968b6e
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-07-22 08:13:06 +0900

    axios api取得

--- React-Learn/naruto-app/src/App.js
+++ React-Learn/naruto-app/src/App.js
@@ -1,10 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  const fetchCharacters = async () => {
+    const apiUrl = "https://narutodb.xyz/api/character";
+
+    const result = await axios.get(apiUrl);
+    console.log(result);
+  };
+
   return (
-    <div className="App">
-      hello world
-    </div>
+    <div className="App">hello world</div>
   );
 }
 
