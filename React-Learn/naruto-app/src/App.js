fdaf93e3dcbbdeeaeb3322207f1677ef09be875e
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-07-22 08:20:49 +0900

    apiから取得した画像を表示

--- React-Learn/naruto-app/src/App.js
+++ React-Learn/naruto-app/src/App.js
@@ -1,8 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
   useEffect(() => {
     fetchCharacters();
   }, []);
@@ -11,11 +12,29 @@ function App() {
     const apiUrl = "https://narutodb.xyz/api/character";
 
     const result = await axios.get(apiUrl);
+    setCharacters(result.data.characters);
     console.log(result);
   };
 
   return (
-    <div className="App">hello world</div>
+    <div className="container">
+      <main>
+        <div className='cards-container'>
+          {characters.map((character) => {
+            return <div className='card' key={character.id}>
+              <img
+                src={character.images[0] != null
+                  ? character.images[0]
+                  : 'dummy.png'
+                }
+                alt='character'
+                className='card-image'
+              />
+            </div>;
+          })}
+        </div>
+      </main>
+    </div>
   );
 }
 
