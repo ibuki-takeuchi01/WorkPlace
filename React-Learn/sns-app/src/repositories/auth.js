936f1bee885cf61af0c1b907fb2302957ccac0da
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-07-28 10:09:54 +0900

    Signin 追加

--- React-Learn/sns-app/src/repositories/auth.js
+++ React-Learn/sns-app/src/repositories/auth.js
@@ -10,4 +10,20 @@ export const authRepository = {
     if (error != null) throw new Error(error.message);
     return { ...data.user, userName: data.user.user_metadata.name };
   },
+  async signin(email, password) {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      if (error) throw new Error(error.message);
+      return {
+        ...data.user,
+        userName: data.user_metadata.name,
+      };
+    } catch (err) {
+      console.error('Login failed:', err.message);
+      throw err;
+    }
+  },
 };
