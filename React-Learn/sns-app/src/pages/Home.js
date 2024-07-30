9eb460a46cd12a074c68a11838543db12bd31cb8
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-07-31 08:01:48 +0900

    ページネーション実装

--- React-Learn/sns-app/src/pages/Home.js
+++ React-Learn/sns-app/src/pages/Home.js
@@ -4,10 +4,13 @@ import { Navigate } from "react-router-dom";
 import { SideMenu } from "../components/SideMenu";
 import { postRepository } from "../repositories/post";
 import { Post } from "../components/Post";
+import { Pagination } from "../components/Pagination";
 
+const limit = 5;
 function Home() {
   const [content, setContent] = useState('');
   const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
   const { currentUser } = useContext(SessionContext);
 
   useEffect(() => {
@@ -22,9 +25,21 @@ function Home() {
     setContent('');
   };
 
-  const fetchPosts = async () => {
-    const posts = await postRepository.find();
+  const fetchPosts = async (page) => {
+    const posts = await postRepository.find(page, limit);
     setPosts(posts);
+  }
+
+  const moveToNext = async () => {
+    const nextPage = page + 1;
+    await fetchPosts(nextPage);
+    setPage(nextPage);
+  }
+
+  const moveToPrev = async () => {
+    const prevPage = page - 1;
+    await fetchPosts(prevPage);
+    setPage(prevPage);
   }
 
   if (currentUser == null) return <Navigate replace to="/signin" />
@@ -62,6 +77,10 @@ function Home() {
           </div>
           <SideMenu />
         </div>
+        <Pagination
+          onPrev={page > 1 ? moveToPrev : null}
+          onNext={posts.length >= limit ? moveToNext : null}
+        />
       </div>
     </div>
   );
