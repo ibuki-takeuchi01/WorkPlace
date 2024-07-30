7650bffdb825e154a42c4ae98fdf8af007f6b7aa
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-07-31 08:20:08 +0900

    ログアウト機能 追加

--- React-Learn/sns-app/src/repositories/auth.js
+++ React-Learn/sns-app/src/repositories/auth.js
@@ -34,6 +34,11 @@ export const authRepository = {
     return {
       ...data.session.user,
       userName: data.session.user.user_metadata.name,
-    }
+    };
+  },
+  async signout() {
+    const { error } = await supabase.auth.signOut();
+    if (error != null) throw new Error(error.message);
+    return true;
   }
 };
