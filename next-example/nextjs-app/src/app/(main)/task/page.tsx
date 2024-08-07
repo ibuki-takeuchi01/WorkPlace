c79e02b25deaf77a1cd3a3d2c68fe86c14dbbbf0
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-08-08 08:29:39 +0900

    ルートハンドラー

--- next-example/nextjs-app/src/app/(main)/task/page.tsx
+++ next-example/nextjs-app/src/app/(main)/task/page.tsx
@@ -1,6 +1,24 @@
-const TaskPage = () => {
+import { Task } from "@/app/api/tasks/route";
+
+const getTass = async () => {
+  const response = await fetch('http://localhost:3000/api/tasks', {
+    method: 'GET',
+    cache: 'no-store'
+  });
+  return await response.json();
+}
+
+const TaskPage = async () => {
+  const tasks = (await getTass()).tasks as Task[];
   return (
-    <div>TaskPage</div>
+    <div>
+      <div>TaskPage</div>
+      <div>
+        {tasks.map((task) => (
+          <div key={task.id}>{task.name}</div>
+        ))}
+      </div>
+    </div>
   )
 }
 
