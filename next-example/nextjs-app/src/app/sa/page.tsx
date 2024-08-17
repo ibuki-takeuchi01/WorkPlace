7c800d241771279eb2b739a56eace394fd46408f
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-08-17 10:34:48 +0900

    ServerActions

--- next-example/nextjs-app/src/app/sa/page.tsx
+++ next-example/nextjs-app/src/app/sa/page.tsx
@@ -1,14 +1,35 @@
+'use client'
+
+import { useFormState, useFormStatus } from 'react-dom'
 import { createTask } from '@/actions/sampleActions'
 import React from 'react'
+import { error } from 'console'
 
 const ServerActionsPage = () => {
   const taskId = 1;
   const createTaskWithTaskId = createTask.bind(null, taskId);
+  const initialState = { error: '' }
+  const [state, formAction] = useFormState(createTaskWithTaskId, initialState);
+
+  const SubmitButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <button
+        type='submit'
+        className='bg-gray-400 ml-2 px-2 disabled:bg-gray-300'
+        disabled={pending}
+      >
+        送信
+      </button>
+    )
+  }
+
   return (
     <div>
-      <form action={createTaskWithTaskId}>
+      <form action={formAction}>
         <input type='text' id='name' name='name' className='bg-gray-200' />
-        <button type='submit' className='bg-gray-400 ml-2 px-2'>送信</button>
+        <SubmitButton />
+        <div>{state.error}</div>
       </form>
     </div>
   )
