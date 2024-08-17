9de8bd789067bc0c4543857a43bceb7725f4ec2f
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-08-17 11:28:23 +0900

    サイドメニュー　実装

---
+++ next-example/task-app/src/components/SideMenu/NavList/NavList.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { FaRegCheckSquare, FaRegClock, FaTasks } from "react-icons/fa";
+
+interface NavItemType {
+  id: number;
+  label: string;
+  link: string;
+  icon: React.ReactNode
+}
+
+const NavList = () => {
+  const navList: NavItemType[] = [
+    { id: 1, label: 'All Tasks', link: '/', icon: <FaTasks className="size-5s" /> },
+    { id: 2, label: 'Completed Tasks', link: '/completed', icon: <FaRegCheckSquare className="size-5s" /> },
+    { id: 3, label: 'Expired Tasks', link: '/expired', icon: <FaRegClock className="size-5s" /> },
+
+  ]
+  return (
+    <div className="mt-24">
+      <div>Link1</div>
+      <div>Link2</div>
+      <div>Link3</div>
+    </div>
+  )
+}
+
+export default NavList
