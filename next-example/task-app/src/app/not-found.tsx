ca5a6f0fe8f8479a21a7cd2b8bb5926dbf7e8100
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-08-18 14:48:35 +0900

    Errorページ　追加

--- next-example/task-app/src/app/not-found.tsx
+++ next-example/task-app/src/app/not-found.tsx
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
       <p className="text-4xl font-medium">Page Not Found</p>
       <Link href="/" className="mt-4 text-xl text-blue-600 hover:underline">Go back home</Link>
     </div>
-  )
+  );
 };
 
 export default NotFoundPage
