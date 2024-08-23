d0afe79b730f2800de1ec83bf8a0d49d3782f704
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-08-24 08:57:09 +0900

    期限切れの期間を修正

--- next-example/task-app/src/components/NewTaskForm/NewTaskForm.tsx
+++ next-example/task-app/src/components/NewTaskForm/NewTaskForm.tsx
@@ -60,7 +60,7 @@ const NewTaskForm = () => {
             type="date"
             id="dueDate"
             name="dueDate"
-            min="2040-01-01"
+            min="2020-01-01"
             max="2100-12-31"
             required
             className="block mt-2 py-1.5 px-2 w-full rounded-md border-0 
