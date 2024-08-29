4bdf4c66b2827e4e58cd10951d80586f4e87e418
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-08-30 08:05:32 +0900

    apiから取得データを画面に表示

--- naruto_app/lib/main.dart
+++ naruto_app/lib/main.dart
@@ -27,6 +27,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _apiUrl = "https://narutodb.xyz/api/character";
+  List<Character> _characters = [];
 
   @override
   void initState() {
@@ -37,18 +38,69 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> _fetchCharacters() async {
     final response = await Dio().get(_apiUrl);
     final List<dynamic> data = response.data["characters"];
-    final characters = data.map((data) => Character.fromJson(data)).toList();
-    print(characters[0].images);
+    setState(() {
+      _characters = data.map((data) => Character.fromJson(data)).toList();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("NARUTO図鑑"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Naruto図鑑'),
+          backgroundColor: Color(0xFFBCE2E8),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: ListView.builder(
+            itemCount: _characters.length,
+            itemBuilder: (context, index) {
+              final character = _characters[index];
+
+              return Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: character.images.isNotEmpty
+                          ? Image.network(
+                              character.images[0],
+                              fit: BoxFit.contain,
+                              errorBuilder: (context, error, stackTrace) {
+                                return const Image(
+                                  image: AssetImage("assets/dummy.png"),
+                                );
+                              },
+                            )
+                          : const Image(
+                              image: AssetImage("assets/dummy.png"),
+                            ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        character.name,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 8.0, bottom: 8.0),
+                      child: Text(
+                        character.debut?["appearsIn"] ?? "なし",
+                        style: const TextStyle(fontSize: 14.0),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
