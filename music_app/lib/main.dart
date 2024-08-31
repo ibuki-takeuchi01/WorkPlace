a7d5a318ab5038c94a6bf6ccb568e1438ff7f6f9
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-08-31 13:42:22 +0900

    APIからアクセストークンを取得

--- music_app/lib/main.dart
+++ music_app/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:music_app/lib/spotify.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
+  getToken();
 }
 
 class MyApp extends StatelessWidget {
