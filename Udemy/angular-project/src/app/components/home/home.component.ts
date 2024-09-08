fa331b5bf1d634c1c979a024287522975f4ae200
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-09-09 08:11:08 +0900

    家電のカテゴリ表示

--- Udemy/angular-project/src/app/components/home/home.component.ts
+++ Udemy/angular-project/src/app/components/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  public categoryList: string[] = ['冷蔵庫', 'トイレ', 'キッチン', '掃除機', '空調機器', 'テレビ', 'その他'];
+
+  public trackByIndex(index: number, item: any): number {
+    return index;
+  }
 }
