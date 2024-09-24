bdb40f796435afdf3aebd4a0704cb745169bc3a9
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-09-25 08:24:10 +0900

    firestoreからデータ取得処理

--- Udemy/household-app/src/App.tsx
+++ Udemy/household-app/src/App.tsx
@@ -6,8 +6,32 @@ import AppLayout from "./components/layout/AppLayout";
 import { theme } from "./theme/theme";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Transaction } from "./types/index";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase"
 
 function App() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Transactions"));
+        const transactionsData = querySnapshot.docs.map((doc) => {
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction
+        });
+        setTransactions(transactionsData);
+      } catch (err) {
+        // TODO: エラー実装
+      }
+    }
+    fetchTransactions();
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
