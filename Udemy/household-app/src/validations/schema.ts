f43fca16f3a7a64cb73dd52d57e9577fabe767b6
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-10-07 07:57:12 +0900

    バリデーションロジックを追加

---
+++ Udemy/household-app/src/validations/schema.ts
@@ -0,0 +1,19 @@
+import { date, z } from "zod";
+
+export const transactionSchema = z.object({
+  type: z.enum(["income", "expense"]),
+  date: z.string().min(1, { message: "日付は必須です" }),
+  amount: z.number().min(1, { message: "金額は1円以上必須です" }),
+  content: z.string().min(1, { message: "内容を入力してください" }),
+
+  category: z.union([
+    z.enum(["食費", "日用品", "住居費", "交際費", "娯楽", "交通費", "医療費"]),
+    z.enum(["給与", "ボーナス", "その他収入"]),
+    z.literal(""),
+  ])
+    .refine((val) => val !== "", {
+      message: "カテゴリを選択してください"
+    })
+});
+
+export type Schema = z.infer<typeof transactionSchema>
