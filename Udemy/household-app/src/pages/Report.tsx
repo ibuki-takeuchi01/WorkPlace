1c6eca523b301f984d360e7c86b6b2e008aabc92
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-10-14 11:25:17 +0900

    レポート画面 レイアウト作成

--- Udemy/household-app/src/pages/Report.tsx
+++ Udemy/household-app/src/pages/Report.tsx
@@ -1,27 +1,33 @@
 import { Grid2, Paper } from '@mui/material'
+import MonthSelector from '../components/MonthSelector'
+import CategoryChart from '../components/CategoryChart'
+import BarChart from '../components/BarChart'
+import TransactionTable from '../components/TransactionTable'
 
 const Report = () => {
   const commonPaperStyle = {
-    height: { xs: "auto", md: "400px" }
+    height: { xs: "auto", md: "400px" },
+    display: "flex",
+    flexDirection: "column",
   }
 
   return (
     <Grid2 container spacing={2}>
       <Grid2 size={{ xs: 12 }}>
-        日付
+        <MonthSelector />
       </Grid2>
       <Grid2 size={{ xs: 12, md: 4 }}>
         <Paper sx={commonPaperStyle}>
-          カテゴリグラフ
+          <CategoryChart />
         </Paper>
       </Grid2>
       <Grid2 size={{ xs: 12, md: 8 }}>
         <Paper sx={commonPaperStyle}>
-          棒グラフ
+          <BarChart />
         </Paper>
       </Grid2>
       <Grid2 size={{ xs: 12 }}>
-        テーブル
+        <TransactionTable />
       </Grid2>
     </Grid2>
   )
