cee2023f13d40094171193a701c18a7796118d00
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-10-14 11:05:33 +0900

    レポート画面 初期レイアウト作成

--- Udemy/household-app/src/pages/Report.tsx
+++ Udemy/household-app/src/pages/Report.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { Grid2, Paper } from '@mui/material'
 
 const Report = () => {
+  const commonPaperStyle = {
+    height: { xs: "auto", md: "400px" }
+  }
+
   return (
-    <div>Report</div>
+    <Grid2 container spacing={2}>
+      <Grid2 size={{ xs: 12 }}>
+        日付
+      </Grid2>
+      <Grid2 size={{ xs: 12, md: 4 }}>
+        <Paper sx={commonPaperStyle}>
+          カテゴリグラフ
+        </Paper>
+      </Grid2>
+      <Grid2 size={{ xs: 12, md: 8 }}>
+        <Paper sx={commonPaperStyle}>
+          棒グラフ
+        </Paper>
+      </Grid2>
+      <Grid2 size={{ xs: 12 }}>
+        テーブル
+      </Grid2>
+    </Grid2>
   )
 }
 
