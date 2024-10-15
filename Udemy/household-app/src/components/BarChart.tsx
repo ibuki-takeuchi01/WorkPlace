70c3cf3360cf236c2b32cb778e7aa42987f73c29
ibuki-takeuchi01 <tomica.0324@gmail.com>
2024-10-16 08:23:32 +0900

    react-chartを使用した棒グラフの実装

--- Udemy/household-app/src/components/BarChart.tsx
+++ Udemy/household-app/src/components/BarChart.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import {
+  Chart as ChartJs,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from 'react-chartjs-2';
+
+ChartJs.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const BarChart = () => {
-  return (
-    <div>BarChart</div>
-  )
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Bar Chart",
+      },
+    },
+  };
+
+  const labels = [
+    "2024-10-15",
+    "2024-10-16",
+    "2024-10-17",
+    "2024-10-18",
+    "2024-10-19",
+    "2024-10-20",
+    "2024-10-21",
+  ]
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "支出",
+        data: [100, 200, 300, 400, 500, 600, 700],
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "収入",
+        data: [900, 800, 700, 600, 500, 400, 300],
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ]
+  }
+
+  return <Bar options={options} data={data} />;
 }
 
 export default BarChart
